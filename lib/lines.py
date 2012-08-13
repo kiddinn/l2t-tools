b2d887665f6624d670db60bbb829283601e91607
Kristinn Gudjonsson <kristinn@log2timeline.net>
2012-08-12 19:40:45 -0700

    Added a new library, defining a Line object and a container for lines. This will be used for duplication removal and checks.

---
+++ lib/lines.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+"""
+This is a file that holds two classes, one to represent a single line (CSV line)
+and another one as a collection of lines, or a container.
+
+Copyright 2012 Kristinn Gudjonsson (kristinn ( a t ) log2timeline (d o t) net)
+
+This file is part of l2t-tools.
+
+    l2t-tools is a collection of free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    l2t-tools is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with log2timeline.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import logging
+
+__author__ = 'Kristinn Gudjonsson (kristinn@log2timeline.net)'
+__version__ = '0.1'
+
+
+class Error(Exception):
+  """Base error handling class."""
+
+
+class DuplicateLine(Error):
+  """Raised when a duplicate line has been detected within a container."""
+
+
+class WrongTimestamp(Error):
+  """Raised when a line with the wrong timestamp is added to a container."""
+
+
+class L2TLine(object):
+  """An object that represents a L2T CSV line."""
+
+  def __init__(self, timestamp, line):
+    """L2TLine is an object that represents a single L2T CSV line.
+
+    Args:
+      timestamp: The constructed timestamp of the entry.
+      line: The entire CSV line as a string.
+    """
+    self.timestamp = timestamp
+    split_entries = line.split(',')
+    self.macb = split_entries[3]
+    self.source = split_entries[4]
+    self.timestamp_desc = split_entries[6]
+    self.source_long = split_entries[5]
+    self.description = split_entries[10]
+
+    # Line split up into sections
+    self.line_first = ','.join(split_entries[0:12])
+    self.line_last = ','.join(split_entries[15:])
+
+    # Features that may be joined..
+    self.filenames = [split_entries[12]]
+    self.inodes = [split_entries[13]]
+    self.notes = split_entries[14]
+
+  def AddFile(self, filename, inode):
+    """Add a file and inode to the filename list."""
+    if type(filename) == str:
+      self.filenames.append(filename)
+    else:
+      self.filenames.extend(filename)
+
+    if type(inode) == list:
+      self.inodes.extend(inode)
+    else:
+      self.inodes.append(str(inode))
+
+  def __str__(self):
+    """Return the line in a string format."""
+    filename = ' '.join(self.filenames)
+    inode = ' '.join(self.inodes)
+    return '{0},{1},{2},{3},{4}'.format(
+        self.line_first, filename, inode, self.notes, self.line_last)
+
+  def __eq__(self, other):
+    if self.timestamp != other.timestamp:
+      return False
+
+    if self.macb != other.macb:
+      return False
+
+    if self.timestamp_desc != other.timestamp_desc:
+      return False
+
+    if self.source_long != other.source_long:
+      return False
+
+    if self.description != other.description:
+      return False
+
+    return True
+
+
+class L2tContainer(object):
+  """A container that stores line objects."""
+
+  def __init__(self):
+    self.lines = []
+    self.timestamp = 0
+
+  def AddLine(self, timestamp, new_line):
+    """Check timestamps values."""
+    if not self.timestamp:
+      self.timestamp = timestamp
+
+    if timestamp != self.timestamp:
+      raise WrongTimestamp('Timestamps changed.')
+
+    # Need to check duplication.
+    line_in = L2TLine(timestamp, new_line)
+    for line in self.lines:
+      if line == line_in:
+        line.AddFile(line_in.filenames, line_in.inodes)
+        raise DuplicateLine('Found a duplicate.')
+
+    self.lines.append(line_in)
+
+  def FlushContainer(self):
+    """Generator."""
+    for line in self.lines:
+      yield line
+
+    self.CloseContainer()
+
+  def __len__(self):
+    return len(self.lines)
+
+  def CloseContainer(self):
+    self.timestamp = 0
+    self.lines = []
+
+
+"""
+Leid til ad taka a duplicate.
+
+ProcessLine
+  Bera saman tima vid buffer
+    b) ekki sami timi, flush buffer.
+try:
+  container.AddLine(line)
+except WrongTimestamp:
+  container.FlushLines()
+  container.AddLine(line)
+
+e-d thannig.
+
+"""
+
---
+++ lib/lines_test.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+"""
+This is a unit test for the lines classes (container and a line).
+
+Copyright 2012 Kristinn Gudjonsson (kristinn ( a t ) log2timeline (d o t) net)
+
+This file is part of l2t-tools.
+
+    l2t-tools is a collection of free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    l2t-tools is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with log2timeline.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import unittest
+
+from l2t_tools.lib import lines
+
+
+class LinesTest(unittest.TestCase):
+  """A simple unit test for the line container and object."""
+
+  def setUp(self):
+    """Set up the needed object.
+
+    We have four lines in total:
+      line1: Original line.
+      line2: Different time and content than line1.
+      line3: Same content as line 1, just different file/inode
+      (should be classified as the same as line1)
+      line4: Same line as line2, except uses the same time as line1.
+    """
+    self.line1 = ("""\
+01/22/2012,07:52:33,UTC,MACB,LOG,Syslog,Entry Written,-,-,\
+[myhostname.myhost.com] Reporter <client> PID: 30840 (INFO \
+No new content.),[myhostname.myhost.com] Reporter <client> PID: \
+30840 (INFO No new content.),2,mysyslog,123,-,Log2t::input::syslog,-""")
+    self.timestamp1 = 20120122075233
+    self.line2 = ("""\
+01/22/2012,07:52:45,UTC,MACB,LOG,Syslog,Entry Written,-,-,\
+[myhostname.myhost.com] Reporter <client> PID: 30840 (INFO No \
+change in [/etc/netgroup]. Done),[myhostname.myhost.com] Reporter \
+<client> PID: 30840 (INFO No change in [/etc/netgroup]. Done),2,\
+mysyslog,123,-,Log2t::input::syslog,-""")
+    self.timestamp2 = 20120122075245
+    self.line3 = ("""\
+01/22/2012,07:52:33,UTC,MACB,LOG,Syslog,Entry Written,-,-,\
+[myhostname.myhost.com] Reporter <client> PID: 30840 (INFO \
+No new content.),[myhostname.myhost.com] Reporter <client> PID: \
+30840 (INFO No new content.),2,othersyslog,128,-,Log2t::input::syslog,-""")
+    self.line4 = ("""\
+01/22/2012,07:52:33,UTC,MACB,LOG,Syslog,Entry Written,-,-,\
+[myhostname.myhost.com] Reporter <client> PID: 30840 (INFO No \
+change in [/etc/netgroup]. Done),[myhostname.myhost.com] Reporter \
+<client> PID: 30840 (INFO No change in [/etc/netgroup]. Done),2,\
+mysyslog,123,-,Log2t::input::syslog,-""")
+    self.line_should_be = ("""\
+01/22/2012,07:52:33,UTC,MACB,LOG,Syslog,Entry Written,-,-,\
+[myhostname.myhost.com] Reporter <client> PID: 30840 (INFO \
+No new content.),[myhostname.myhost.com] Reporter <client> PID: \
+30840 (INFO No new content.),2,mysyslog othersyslog,123 128,-,\
+Log2t::input::syslog,-""")
+
+  def testLine(self):
+    """Do a simple test for the line objects."""
+    line1 = lines.L2TLine(self.timestamp1, self.line1)
+    line2 = lines.L2TLine(self.timestamp2, self.line2)
+    line3 = lines.L2TLine(self.timestamp1, self.line3)
+
+    self.assertEquals(line1.timestamp, self.timestamp1)
+    self.assertEquals(line2.timestamp, self.timestamp2)
+    self.assertEquals(str(line1), self.line1)
+    self.assertEquals(str(line2), self.line2)
+
+    # Append a filename.
+    line1.AddFile('othersyslog', 128)
+    self.assertEquals(str(line1), self.line_should_be)
+
+    self.assertFalse(line1 == line2)
+    self.assertTrue(line1 == line3)
+
+
+  def testContainer(self):
+    """Test L2tContainer with various operations against it."""
+    container = lines.L2tContainer()
+
+    container.AddLine(self.timestamp1, self.line1)
+    self.assertEquals(len(container), 1)
+
+    self.assertRaises(lines.WrongTimestamp, container.AddLine, self.timestamp2, self.line2)
+    container.AddLine(self.timestamp1, self.line4)
+    self.assertEquals(len(container), 2)
+
+    # Add line2, which should raise the "the same" flag.
+    self.assertRaises(lines.DuplicateLine, container.AddLine, self.timestamp1, self.line3)
+
+    all_lines = list(container.FlushContainer())
+    self.assertEquals(len(all_lines), 2)
+
+    self.assertEquals(str(all_lines[0]), self.line_should_be)
+    self.assertEquals(str(all_lines[1]), self.line4)
+
+
+if __name__ == '__main__':
+  unittest.main()
